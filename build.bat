bc6ad8368f5c86647f7039b5e9533eaa7cd606c1
Solra Bizna <solra@bizna.name>
2022-05-29 21:22:49 -0600

    Make it build on more recent WLA

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-wla-65c02 -qo ocmosbios.65c ocmosbios.o
-wlalink -vS ocmosbios.link ocmosbios.rom
+wla-65c02 -o ocmosbios.o ocmosbios.65c
+wlalink -v ocmosbios.link ocmosbios.rom
 del ocmosbios.cabe
 lua rom_to_cabe.lua ocmosbios.rom ocmosbios.cabe
--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-wla-65c02 -qo ocmosbios.65c ocmosbios.o
-wlalink -vS ocmosbios.link ocmosbios.rom
+wla-65c02 -o ocmosbios.o ocmosbios.65c
+wlalink -v ocmosbios.link ocmosbios.rom
 
 rm -f ocmosbios.cabe
 ./rom_to_cabe.lua ocmosbios.rom ocmosbios.cabe || (echo "Failed to create the CABE image. That's okay, use the raw ROM instead."; true)
